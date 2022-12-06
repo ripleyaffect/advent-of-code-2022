90db459834b52eb4077b9a06c68f91e7473820a0
ripleyaffect <t.ripley.peterson@gmail.com>
2022-12-06 11:59:58 -0700

    Days 5 and 6

--- src/days/day_05/mod.rs
+++ src/days/day_05/mod.rs
@@ -1,3 +1,6 @@
+use regex::Regex;
+use crate::utils;
+
 pub fn get_part(part: u8) -> i32 {
     match part {
         1 => part_1(),
@@ -6,6 +9,127 @@ pub fn get_part(part: u8) -> i32 {
     }
 }
 
-fn part_1() -> i32 { 0 }
+fn part_1() -> i32 {
+    let mut stacks = get_initial_stacks();
 
-fn part_2() -> i32 { 0 }
+    let input_lines = utils::read_input_lines("05");
+    for line in input_lines {
+        let instruction = Instruction::from(parse_line(&line));
+        perform_instruction(&mut stacks, instruction);
+    }
+    print_stack_tops(&stacks);
+
+    0
+}
+
+fn part_2() -> i32 {
+    let mut stacks = get_initial_stacks();
+
+    let input_lines = utils::read_input_lines("05");
+    for line in input_lines {
+        let instruction = Instruction::from(parse_line(&line));
+        perform_instruction_two(&mut stacks, instruction);
+    }
+    print_stack_tops(&stacks);
+
+    0
+}
+
+struct Instruction {
+    amount: usize,
+    from: usize,
+    to: usize,
+}
+
+impl Instruction {
+    fn from(vals: (usize, usize, usize)) -> Instruction {
+        Instruction {
+            amount: vals.0,
+            from: vals.1,
+            to: vals.2,
+        }
+    }
+}
+
+// [D]
+// [N] [C]
+// [Z] [M] [P]
+//  1   2   3
+fn get_test_stacks() -> Vec<Vec<char>> {
+    let mut stacks = Vec::new();
+
+    stacks.push(vec![]);
+    stacks.push("ZN".chars().collect());
+    stacks.push("MCD".chars().collect());
+    stacks.push("P".chars().collect());
+
+    stacks
+}
+
+//                [J] [Z] [G]
+//                [Z] [T] [S] [P] [R]
+//    [R]         [Q] [V] [B] [G] [J]
+//    [W] [W]     [N] [L] [V] [W] [C]
+//    [F] [Q]     [T] [G] [C] [T] [T] [W]
+//    [H] [D] [W] [W] [H] [T] [R] [M] [B]
+//    [T] [G] [T] [R] [B] [P] [B] [G] [G]
+//    [S] [S] [B] [D] [F] [L] [Z] [N] [L]
+//     1   2   3   4   5   6   7   8   9
+fn get_initial_stacks() -> Vec<Vec<char>> {
+    let mut stacks = Vec::new();
+
+    stacks.push(vec![]);
+    stacks.push("STHFWR".chars().collect());
+    stacks.push("SGDQW".chars().collect());
+    stacks.push("BTW".chars().collect());
+    stacks.push("DRWTNQZJ".chars().collect());
+    stacks.push("FBHGLVTZ".chars().collect());
+    stacks.push("LPTCVBSG".chars().collect());
+    stacks.push("ZBRTWGP".chars().collect());
+    stacks.push("NGMTCJR".chars().collect());
+    stacks.push("LGBW".chars().collect());
+
+    stacks
+}
+
+fn parse_line(line: &String) -> (usize, usize, usize) {
+    let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+    let cap = re.captures(line).unwrap();
+    (
+        cap[1].parse().unwrap(),
+        cap[2].parse().unwrap(),
+        cap[3].parse().unwrap(),
+    )
+}
+
+fn perform_instruction(stacks: &mut Vec<Vec<char>>, instruction: Instruction) -> &mut Vec<Vec<char>> {
+    let mut from_stack = stacks.get(instruction.from).cloned().unwrap();
+    let mut chars: Vec<char> = from_stack.iter().rev().take(instruction.amount).cloned().collect();
+    from_stack.truncate(from_stack.len() - instruction.amount);
+    stacks[instruction.from] = from_stack;
+
+    let to_stack = stacks.get_mut(instruction.to).unwrap();
+    to_stack.append(&mut chars);
+
+    stacks
+}
+
+fn perform_instruction_two(stacks: &mut Vec<Vec<char>>, instruction: Instruction) -> &mut Vec<Vec<char>> {
+    let mut from_stack = stacks.get(instruction.from).cloned().unwrap();
+    let mut chars: Vec<char> = from_stack.iter().rev().take(instruction.amount).rev().cloned().collect();
+    from_stack.truncate(from_stack.len() - instruction.amount);
+    stacks[instruction.from] = from_stack;
+
+    let to_stack = stacks.get_mut(instruction.to).unwrap();
+    to_stack.append(&mut chars);
+
+    stacks
+}
+
+fn print_stack_tops(stacks: &Vec<Vec<char>>) {
+    let mut chars: Vec<&char> = Vec::new();
+    for stack in stacks[1..].iter() {
+        chars.push(stack.last().unwrap())
+    }
+    println!("{:?}", chars.into_iter().collect::<String>());
+}
--- src/days/day_06/mod.rs
+++ src/days/day_06/mod.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+use crate::utils;
+
 pub fn get_part(part: u8) -> i32 {
     match part {
         1 => part_1(),
@@ -6,6 +9,27 @@ pub fn get_part(part: u8) -> i32 {
     }
 }
 
-fn part_1() -> i32 { 0 }
+fn part_1() -> i32 {
+    let input_lines = utils::read_input_lines("06");
+    let chars: Vec<char> = input_lines[0].chars().collect();
+    get_start(chars, 4)
+}
 
-fn part_2() -> i32 { 0 }
+fn part_2() -> i32 {
+  let input_lines = utils::read_input_lines("06");
+  let chars: Vec<char> = input_lines[0].chars().collect();
+  get_start(chars, 14)
+}
+
+fn get_start(chars: Vec<char>, needed: usize) -> i32 {
+  let mut idx = 0;
+  while !is_unique(&chars[idx..=idx+(needed-1)]) {
+      idx += 1;
+  }
+  (idx + needed) as i32
+}
+
+fn is_unique(slice: &[char]) -> bool {
+    let set: HashSet<&char> = HashSet::from_iter(slice);
+    set.len().eq(&slice.len())
+}
